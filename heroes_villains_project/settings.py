96047083fd70bf871e384acdd355c1aad762072f
Kendall Morgan <kuyukip@gmail.com>
2022-05-28 11:25:38 -0500

    created local settings and moved created database over as well as migrated to mysql

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-urd83=lp7==h=#7eu9&d+4h!v*v&82345cuj0eft^1d052c@wd'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -70,17 +67,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'heroes_villains_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -121,3 +107,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
