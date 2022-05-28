7926a6e2b914a7b402184fb4cdfca00300e172c8
Kendall Morgan <kuyukip@gmail.com>
2022-05-28 12:12:05 -0500

    created get put post and delete on view and all of the proper messages

--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,39 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperSerializer
+from .models import Super
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
 
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
 
-    return Response('ok')
+
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def super_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(Super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
